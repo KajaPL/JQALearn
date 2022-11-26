3b4a45d47691aa3befc3296ed832864000750447
KajaPL <116584488+KajaPL@users.noreply.github.com>
2022-11-26 17:38:50 +0200

    Homework6 solution

--- src/main/java/Homework2/Homework6.java
+++ src/main/java/Homework2/Homework6.java
@@ -1,6 +1,19 @@
 package Homework2;
 
+import java.util.Arrays;
+
 public class Homework6 {
+    public static void main(String[] args) {
+        int[] firstArr = {1, 2, 3};
+        int[] secondArr = {1, 2, 3};
+
+        if (Arrays.equals(firstArr, secondArr)) {
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays are not equal");
+        }
+    }
     /*Задача 6
-Дадени са два масива със стойности {1, 2, 3} и {1, 2, 3} да се състави програма, която проверява дали масивите са еднакви*/
+Дадени са два масива със стойности {1, 2, 3} и {1, 2, 3} да се състави програма,
+която проверява дали масивите са еднакви*/
 }
