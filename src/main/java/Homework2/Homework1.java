71330996dd4ce4e206928c43a97f5c3729ac256a
KajaPL <116584488+KajaPL@users.noreply.github.com>
2022-11-26 15:46:36 +0200

    Homework1 solution

--- src/main/java/Homework2/Homework1.java
+++ src/main/java/Homework2/Homework1.java
@@ -1,6 +1,25 @@
 package Homework2;
 
+import java.util.Scanner;
+
 public class Homework1 {
+    static int sumOfTwoInputNums(int firstNum, int secondNum){
+    int sumOfNums = firstNum + secondNum;
+    return sumOfNums;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first int input num:");
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter second int input num:");
+        int secondNum = Integer.parseInt(scanner.nextLine());
+        System.out.println("Sum of your two input int nums is: " + sumOfTwoInputNums(firstNum, secondNum));
+        //when you write in () it is already without int
+    }
+
     /*Задача 1:
-Да се състави метод, който приема като аргументи две числа и отпечатва на екрана техния сбор.*/
+Да се състави метод, който приема като аргументи две числа
+и отпечатва на екрана техния сбор.*/
 }
