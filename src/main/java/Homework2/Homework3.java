264d91f851ab1e62401f795ec96a74c08c86eea0
KajaPL <116584488+KajaPL@users.noreply.github.com>
2022-11-26 16:31:50 +0200

    Homework3 solution

--- src/main/java/Homework2/Homework3.java
+++ src/main/java/Homework2/Homework3.java
@@ -1,6 +1,25 @@
 package Homework2;
 
+import java.util.Scanner;
+
 public class Homework3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = new int[5];
+
+        System.out.println("Enter five input int nums:");
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = Integer.parseInt(scanner.nextLine());
+        }
+   //you can also use i <= 4 instead of array.length
+
+        System.out.println("Stored in array your input int nums are:");
+        for (int i = 0; i <=array.length-1; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
     /*Задача 3
-Да се прочете масив от екрана с 5 числа по избор от тип double и да се отпечатат всички резултати*/
+Да се прочете масив от екрана с 5 числа по избор от тип double
+и да се отпечатат всички резултати*/
 }
