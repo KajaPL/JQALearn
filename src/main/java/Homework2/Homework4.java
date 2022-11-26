6d5398be0adb2d95c5ba556bd103b5ad5641f17b
KajaPL <116584488+KajaPL@users.noreply.github.com>
2022-11-26 18:18:46 +0200

    Homework4 solution

--- src/main/java/Homework2/Homework4.java
+++ src/main/java/Homework2/Homework4.java
@@ -1,6 +1,22 @@
 package Homework2;
 
 public class Homework4 {
+    public static void main(String[] args) {
+        int [] arr = {9, 19, 29, 39, 49, 59, 69, 79, 89, 99};
+
+        //for
+        System.out.println("Here is traverse of array using for loop: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        //while
+        int index = 0;
+        System.out.println("\n\nHere is traverse of array using while: ");
+        while (index < arr.length) {
+            System.out.print(arr[index] + " ");
+            index++;
+        }
+    }
     /*Задача 4
 Напишете програма, която създава масив с 10 елемента от тип int и обходете масива:
 1) с помоща на for loop
