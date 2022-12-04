01a2e544ce8ea9848714001a7917b1032bf91c7d
KajaPL <116584488+KajaPL@users.noreply.github.com>
2022-12-04 13:34:29 +0200

    Homework3 Car and CarPerson still not finished

--- src/main/java/Homework3/Car.java
+++ src/main/java/Homework3/Car.java
@@ -1,5 +1,7 @@
 package Homework3;
 
+import java.util.Scanner;
+
 public class Car {
     int year;
     double price;
@@ -7,6 +9,29 @@ public class Car {
     int fuelTankCapacity;
     double freeFuel;
     String engineFuelOperationSystem;
+
+public void changeEngineFuelOperationSystem(String newEngineFuelOperationSystem){
+    this.engineFuelOperationSystem = newEngineFuelOperationSystem;
+    System.out.println("System changed to: " + engineFuelOperationSystem);
+    }
+public void useFuel(double fuel){
+    if (this.freeFuel < fuel) {
+        this.freeFuel =0;
+        System.out.println("Not enough free fuel!");
+    } else {
+        this.freeFuel = freeFuel - fuel;
+        System.out.println("Free fuel changed to:" + this.freeFuel);
+    }
+}
+
+public void printingCars () {
+    System.out.println("\n\nManufactured date: " + year
+            + "\nPrice is: " + price
+            + "\nThis is sport car: " + isSportCar
+            + "\nFuel tank capacity is:" + fuelTankCapacity
+            + "\nFree fuel is:" + freeFuel
+            + "\nEngine fuel operations system is:" + engineFuelOperationSystem);
+}
 
 /*Създайте клас Car, който представя кола.
 Класът да има следните полета:
@@ -23,8 +48,12 @@ engineFuelOperationSystem – текстово поле за вида горив
 Класът да дефинира следните методи:
 -метод void changeЕngineFuelOperationSystem(String newЕngineFuelOperationSystem), който сменя
 стойността на полето engineFuelOperationSystem със стойността подадена като параметър.
+
 -метод void useFuel(double fuel), който намалява свободното гориво
 (freeFuel) със стойността подадена като аргумент.
+
 Ако стойността на аргумента е по-голяма от наличното гориво, извеждa
-съобщение "Not enough free fuel!"*/
+съобщение "Not enough free fuel!"
+
+след което да се изведат на екрана всичките полета на дете коли*/
 }
--- src/main/java/Homework3/CarPerson.java
+++ src/main/java/Homework3/CarPerson.java
@@ -1,5 +1,7 @@
 package Homework3;
 
+import java.util.Scanner;
+
 public class CarPerson {
     public static void main(String[] args) {
         Car infiniti = new Car();
@@ -7,24 +9,44 @@ public class CarPerson {
         infiniti.price = 55.555;
         infiniti.isSportCar=true;
         infiniti.fuelTankCapacity = 100;
-        infiniti.freeFuel = 10;
+        infiniti.freeFuel = 53;
         infiniti.engineFuelOperationSystem = "Diesel";
+
 
         Car golf = new Car ();
         golf.year = 1995;
         golf.isSportCar=false;
         golf.fuelTankCapacity = 50;
         golf.price = 1.111;
-        golf.freeFuel = 2;
-        golf.engineFuelOperationSystem = "Gasoline";
+        golf.freeFuel = 35;
+        golf.engineFuelOperationSystem = "CNG";
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please write engine fuel. You can choose only one: petrol, cng, diesel, electricity, biofuels:");
+        String newEngineFuelOperationSystem = scanner.nextLine();
+        infiniti.changeEngineFuelOperationSystem(newEngineFuelOperationSystem);
+
+        System.out.println("Please write a double num:");
+        double fuelChanged = Double.parseDouble(scanner.nextLine());
+        golf.useFuel(fuelChanged);
+
+        infiniti.printingCars();
+        golf.printingCars();
     }
 
         /*Да се създаде клас CarPerson, с main метод.
 В рамките на main метода да се създадат 2 обекта от тип Car.
 
 Да се зададат стойности на всеки от колите за year, price,
-fuelTankCapacity, freeFuel, engineFuelOperationSystem.Нека едината кола да е спортна. На едната от колата да е заделено 35 литра гориво (чрез
-метода useFuel), а на другия, да се смени горивната система (чрез
-метода changeЕngineFuelOperationSystem), след което да се изведат на екрана всичките полета на дете коли*/
+fuelTankCapacity, freeFuel, engineFuelOperationSystem.
+
+Нека едината кола да е спортна.
+
+На едната от колата да е заделено 35 литра гориво (чрез
+метода useFuel),
+
+а на другия, да се смени горивната система (чрез
+метода changeЕngineFuelOperationSystem),
+*/
 }
