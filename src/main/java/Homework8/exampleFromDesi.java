4481703600b7703e4aeb03d489f9cede38562d86
kajam <caia.balcania@gmail.com>
2023-02-15 18:56:47 +0200

    homework8 example from Desi os: mac chrome: 110 selenium 4.8.0

---
+++ src/main/java/Homework8/exampleFromDesi.java
@@ -0,0 +1,32 @@
+package Homework8;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+public class exampleFromDesi {
+    public static void main(String[] args) {
+       /*
+       Expected result: After clicking on login btn,
+       page should reload and text should appear
+       "The password that you've entered is incorrect. Forgotten password?"
+       */
+
+        // System.setProperty("webdriver.chrome.driver", "F:\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/kajam/Downloads/chromedriver_mac_arm64 2/chromedriver");
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.facebook.com/");
+        WebElement element = driver.findElement(By.xpath("//input[@name='email']"));
+        element.sendKeys("abc@gmail.com");
+        element = driver.findElement(By.xpath("//input[@name='pass']"));
+        element.sendKeys("abbaas");
+
+        WebElement button = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]"));
+        button.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        button = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button"));
+        button.click();
+    }
+}
