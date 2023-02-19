9a9789c63708d2cf80c898652d844e6eb45cc620
kajam <caia.balcania@gmail.com>
2023-02-19 14:09:22 +0200

    homework8 ftc_login os: mac chrome: 110 selenium 4.8.0

--- src/main/java/Homework8/FirstTestCase_login.java
+++ src/main/java/Homework8/FirstTestCase_login.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTestCase_login {
     public static void main(String[] args) {
+        //Verify that login btn is working
+
         //path of the browser driver
         System.setProperty("webdriver.chrome.driver", "/Users/kajam/Downloads/chromedriver_mac_arm64 2/chromedriver");
         //Webdriver instance
