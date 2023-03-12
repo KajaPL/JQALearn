64a6ee4515eb74b4eadfe7f577188ce51276180d
kajam <caia.balcania@gmail.com>
2023-03-12 12:57:19 +0200

    Homework 11 ADDED GateRegisterPF.java  - TO DO more exercises

---
+++ src/main/java/Homework11/pages/GateRegisterPF.java
@@ -0,0 +1,8 @@
+package Homework11.pages;
+
+public class GateRegisterPF {
+    //TO DO
+    //MORE EXERCISES
+    // HERE I'LL CHECK IF ALL ELEMENTS ARE VISIBLE AND/OR CLICKABLE
+    // ON THE GATE - REGISTER FORM
+}
