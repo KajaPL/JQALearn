076d163b9b6d1e2a53529892d942a7d6445105b1
kajam <caia.balcania@gmail.com>
2023-03-12 12:59:17 +0200

    Contents are identical

--- src/main/java/Homework10/pages/GatePF.java
+++ src/main/java/Homework10/pages/GatePF.java
@@ -1,15 +1,11 @@
 package Homework10.pages;
 
-import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.time.Duration;
 
 public class GatePF {
     WebDriver driver;
@@ -23,7 +19,6 @@ public class GatePF {
 
    //click on login btn
     public void clickLoginBtn() {
-       // loginBtn.click();
         Actions builder = new Actions(driver);
         Action click = builder
                 .click(loginBtn)
--- src/main/java/Homework10/tests/GateTestsPF.java
+++ src/main/java/Homework10/tests/GateTestsPF.java
@@ -4,12 +4,9 @@ import Homework10.library.BrowserFactory;
 import Homework10.pages.GatePF;
 import org.junit.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class GateTestsPF {
