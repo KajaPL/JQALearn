7813d20949741f0929d55ae4bcf84fb731564861
kajam <caia.balcania@gmail.com>
2023-03-12 12:36:49 +0200

    Homework 11 GateTestsPF.java added with explicit wait and expected condition

---
+++ src/main/java/Homework11/tests/GateTestsPF.java
@@ -0,0 +1,61 @@
+package Homework11.tests;
+
+import Homework11.library.BrowserFactory;
+import Homework11.pages.GatePF;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class GateTestsPF extends BrowserFactory{
+    static WebDriver driver;
+    GatePF objGatePf;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception{
+        System.out.println("This is @BeforeClass");
+        // in this way I take chrome from BrowserFactory.java
+        // remember - it should be imported
+        driver =  BrowserFactory.getBrowser("Chrome");
+    }
+    @Before
+    public void before() {
+        System.out.println("This is @Before");
+        //maximize window
+        driver.manage().window().maximize();
+        //launch site
+        driver.get("http://arenamody.pl");
+    }
+
+    @After
+    public void after() {
+        System.out.println("This is @After");
+    }
+    @AfterClass public static void afterClass() throws Exception{
+
+        System.out.println("This is @AfterClass");
+        BrowserFactory.closeAllDriver();
+    }
+
+
+    @Test
+    public void verifyThatLoginBtnCanBeClicked(){
+        System.out.println("This is @Test 1");
+        objGatePf = new GatePF(driver);
+        objGatePf.clickLoginBtn();
+        //explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //expected condition for wait
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login")));
+        //verify result with assert
+        WebElement popup = driver.findElement(By.id("login"));
+        boolean isPopupVisible = driver.findElement(By.id("login")).isDisplayed();
+        System.out.println(isPopupVisible);
+        Assert.assertTrue(isPopupVisible);
+    }
+}
