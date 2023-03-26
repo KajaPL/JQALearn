9f1e7a24a28094b727ea4841fb20eff78917b4d4
kajam <caia.balcania@gmail.com>
2023-03-26 22:05:22 +0300

    TestRunner.java added

---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,23 @@
+package testRunner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.Scenario;
+import java.util.Map;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = true,
+        features = {"src/test/resources/featureFiles/SwagLabsLogin.feature"},
+        glue = { "stepDefinition", "testRunner" },
+        tags = {"@login"},
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        monochrome = true
+)
+
+public class TestRunner extends Hooks{
+
+}
