7b4c22a0ee6d18a7e74014dd427b72f8bf00a332
kajam <caia.balcania@gmail.com>
2023-04-02 23:51:07 +0300

    LoginPage.java updated findy by id

--- src/test/java/PageObjectModels/LoginPage.java
+++ src/test/java/PageObjectModels/LoginPage.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
      WebDriver driver;
-    @FindBy(css = "#user-name")
+    @FindBy(id = "user-name")
     WebElement standardUser;
-    @FindBy(css = "#password")
+    @FindBy(id = "password")
     WebElement secretSauce;
-    @FindBy(css = "#login-button")
+    @FindBy(id = "login-button")
     WebElement loginButton;
 
     public LoginPage(WebDriver driver){
