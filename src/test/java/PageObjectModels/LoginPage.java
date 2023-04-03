e4d84b73c84c27ba165d0ad4bb6789df4d03fcc1
kajam <caia.balcania@gmail.com>
2023-04-03 13:48:53 +0300

    LoginPage updated

--- src/test/java/PageObjectModels/LoginPage.java
+++ src/test/java/PageObjectModels/LoginPage.java
@@ -1,28 +1,58 @@
 package PageObjectModels;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 
 public class LoginPage {
-     WebDriver driver;
+    WebDriver driver;
     @FindBy(id = "user-name")
-    WebElement standardUser;
+    WebElement username;
     @FindBy(id = "password")
-    WebElement secretSauce;
+    WebElement password;
     @FindBy(id = "login-button")
-    WebElement loginButton;
+    WebElement loginBtn;
 
     public LoginPage(WebDriver driver){
         this.driver = driver;
-        PageFactory.initElements(this.driver, this);
+        PageFactory.initElements(driver, this);
     }
-    public void enterNameAndPassword(String standardUser, String secretSauce){
-        this.standardUser.sendKeys(standardUser);
-        this.secretSauce.sendKeys(secretSauce);
+    public void validateURL(String expectedUrl) {
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
+    }
+
+    public void validateTitle(String expectedTitle) {
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+    }
+
+    public void enterUsernameAndPassword(String standardUser, String secretSauce){
+        username.sendKeys(standardUser);
+        password.sendKeys(secretSauce);
+    }
+
+    public void waitForLoginButton() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //expected condition for wait
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
+
     }
     public void clickLoginButton(){
-        this.loginButton.click();
+        Actions builder = new Actions(driver);
+        Action click = builder
+                .click(loginBtn)
+                .build();
+        click.perform();
     }
 }
