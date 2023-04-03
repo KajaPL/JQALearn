b93ca007a433bd265d63feeffad6350709dd7ae9
kajam <caia.balcania@gmail.com>
2023-04-03 13:49:14 +0300

    WelcomePage updated

--- src/test/java/PageObjectModels/WelcomePage.java
+++ src/test/java/PageObjectModels/WelcomePage.java
@@ -1,24 +1,36 @@
 package PageObjectModels;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
 
 public class WelcomePage {
     WebDriver driver;
-    @FindBy(className = "title")
+    @FindBy(id = "inventory_container")
     WebElement inventoryContainer;
     public WelcomePage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
+    public void validateURL(String expectedUrl) {
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
+    }
     public void productList() throws Exception{
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        this.inventoryContainer.getText();
-        System.out.println(inventoryContainer);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //expected condition for wait
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inventory_container")));
+        boolean isInventoryContainerVisible = inventoryContainer.isDisplayed();
+        Assert.assertTrue(isInventoryContainerVisible);
+
     }
 }
